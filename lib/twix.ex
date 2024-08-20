1e302dd06fd828073234b7bbc906fc5a1bcc1322
UrquizaN <urquiza-nobrega@outlook.com>
2024-08-20 06:24:49 -0300

    feat: delegate routes

--- lib/twix.ex
+++ lib/twix.ex
@@ -6,4 +6,13 @@ defmodule Twix do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+  alias Twix.Posts
+  alias Twix.Users
+
+  defdelegate create_post(params), to: Posts.Create, as: :call
+  defdelegate add_like_to_post(id), to: Posts.AddLike, as: :call
+
+  defdelegate create_user(params), to: Users.Create, as: :call
+  defdelegate get_user(id), to: Users.Get, as: :call
+  defdelegate update_user(params), to: Users.Update, as: :call
 end
