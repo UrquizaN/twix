50111f80ed383c800e6cc8705555923e780e69c1
UrquizaN <urquiza-nobrega@outlook.com>
2024-09-18 07:25:22 -0300

    feat: add crudry to handle error messages

--- lib/twix_web/schema/types/root.ex
+++ lib/twix_web/schema/types/root.ex
@@ -3,6 +3,7 @@ defmodule TwixWeb.Schema.Types.Root do
 
   import_types TwixWeb.Schema.Types.User
 
+  alias Crudry.Middlewares.TranslateErrors
   alias TwixWeb.Resolvers.User, as: UserResolver
 
   object :root_query do
@@ -18,12 +19,14 @@ defmodule TwixWeb.Schema.Types.Root do
       arg :input, type: non_null(:create_user_input)
 
       resolve &UserResolver.create/2
+      middleware TranslateErrors
     end
 
     field :update_user, type: :user do
       arg :input, type: non_null(:update_user_input)
 
       resolve &UserResolver.update/2
+      middleware TranslateErrors
     end
   end
 end
